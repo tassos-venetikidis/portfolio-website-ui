92a11b282e088ff061dd562142e2805ae7e1f010
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-06-23 17:00:21 +0300

    Imported and applied the Project type

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,3 +1,4 @@
+import type { Project } from "~/types";
 import type { Route } from "./+types/index";
 
 export function meta({}: Route.MetaArgs) {
@@ -12,14 +13,16 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
   const res = await fetch("http://localhost:8000/projects");
   const data = await res.json();
   return { projects: data };
 }
 
 function ProjectsPage({ loaderData }: Route.ComponentProps) {
-  const { projects } = loaderData;
+  const { projects } = loaderData as { projects: Project[] };
   console.log(projects);
   return (
     <>
