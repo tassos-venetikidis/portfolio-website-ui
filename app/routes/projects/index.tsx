d4e0884a2b354ad5678beb6999e13c0810547280
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-06-23 16:45:35 +0300

    Implemented a loader in the projects index route that fetches projects from the json-server

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -12,7 +12,15 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-function ProjectsPage() {
+export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+  const res = await fetch("http://localhost:8000/projects");
+  const data = await res.json();
+  return { projects: data };
+}
+
+function ProjectsPage({ loaderData }: Route.ComponentProps) {
+  const { projects } = loaderData;
+  console.log(projects);
   return (
     <>
       <h2 className="text-3xl font-bold text-white mb-8">🚀 Projects</h2>
