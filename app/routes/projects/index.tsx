03acf1a814b8320780da330016e422d97a08336a
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-06-25 00:13:56 +0300

    Implemented a ProjectCard component and applied it for the display of projects in the projects index page

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,5 +1,6 @@
 import type { Project } from "~/types";
 import type { Route } from "./+types/index";
+import ProjectCard from "~/components/ProjectCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -23,10 +24,15 @@ export async function loader({
 
 function ProjectsPage({ loaderData }: Route.ComponentProps) {
   const { projects } = loaderData as { projects: Project[] };
-  console.log(projects);
+
   return (
     <>
       <h2 className="text-3xl font-bold text-white mb-8">🚀 Projects</h2>
+      <div className="grid gap-6 sm:grid-cols-2">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </>
   );
 }
